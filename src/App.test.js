5bd70ddee819d0000fafa9d5e550c3e88a343002
Marcin Piłka <m.pilka@propellerads.net>
2018-08-15 22:09:51 +0200

    App test fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import configureStore from 'redux-mock-store';
+import {initialState} from "./redux/reducers";
+import {Provider} from "react-redux";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
-});
+describe('Test App component', () => {
+    const mockStore = configureStore();
+    let store;
+
+    beforeEach(() => {
+        store = mockStore(initialState);
+    });
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Provider store={store}><App/></Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+})
