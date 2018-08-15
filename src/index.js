a56430be1f68ae70a551e5c743f104f0f98fda79
Marcin Piłka <m.pilka@propellerads.net>
2018-08-15 21:32:27 +0200

    style fixes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 
 
-const store = createStore(currenciesReducer, undefined, compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const store = createStore(currenciesReducer, undefined, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
